085ae156e257fe071421ee74db8c9aba4f843a22
Amira <amira.saad@esprit.tn>
2020-10-02 18:12:52 +0100

    ImplementingRouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { ProductListComponent } from './components/shopping-cart/product-list/pr
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AppRoutingModule } from './app-routing.modules';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,11 +26,15 @@ import { ProductItemComponent } from './components/shopping-cart/product-list/pr
     ProductListComponent,
     CartComponent,
     CartItemComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
